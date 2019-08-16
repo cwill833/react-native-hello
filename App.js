702ec8410dfe461bf184d1a632fea1e4decf000f
Chris Williams <cwill833@gmail.com>
2019-08-16 17:20:15 -0400

    switched to class component instead of functional

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component}from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Does this also change the app text?</Text>
+        <Text>Liza come check out my app!!!!!</Text>
+        <Text>Still work?</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
